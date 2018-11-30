53b9be0402b2087ef2ca36b1199d63b0da554d89
Yong Li <yong.li@intel.com>
2018-11-30 09:20:29 +0800

    Fix the utf encode issue

--- test.py
+++ test.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python
+#coding:utf-8
+
 import numpy as np
 import sys, os
 import time
@@ -8,13 +11,29 @@ sys.path.append(os.getcwd())
 # crnn packages
 import torch
 from torch.autograd import Variable
+
+
+# Monkey-patch because I trained with a newer version.
+# This can be removed once PyTorch 0.4.x is out.
+# See https://discuss.pytorch.org/t/question-about-rebuild-tensor-v2/14560
+import torch._utils
+try:
+    torch._utils._rebuild_tensor_v2
+except AttributeError:
+    def _rebuild_tensor_v2(storage, storage_offset, size, stride, requires_grad, backward_hooks):
+        tensor = torch._utils._rebuild_tensor(storage, storage_offset, size, stride)
+        tensor.requires_grad = requires_grad
+        tensor._backward_hooks = backward_hooks
+        return tensor
+    torch._utils._rebuild_tensor_v2 = _rebuild_tensor_v2
+
 import utils
 import dataset
 from PIL import Image
 import models.crnn as crnn
 import alphabets
 str1 = alphabets.alphabet
-
+print(str1)
 import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('--images_path', type=str, default='test_images/test1.png', help='the path to your images')
@@ -24,11 +43,13 @@ opt = parser.parse_args()
 # crnn params
 # 3p6m_third_ac97p8.pth
 crnn_model_path = 'trained_models/mixed_second_finetune_acc97p7.pth'
-alphabet = str1
+alphabet = str1.decode('utf-8')
 nclass = len(alphabet)+1
+#nclass = 6736
 
+print(nclass)
 
-# crnn文本信息识别
+# crnn 中
 def crnn_recognition(cropped_image, model):
 
     converter = utils.strLabelConverter(alphabet)
@@ -51,8 +72,17 @@ def crnn_recognition(cropped_image, model):
     preds = preds.transpose(1, 0).contiguous().view(-1)
 
     preds_size = Variable(torch.IntTensor([preds.size(0)]))
-    sim_pred = converter.decode(preds.data, preds_size.data, raw=False)
-    print('results: {0}'.format(sim_pred))
+    i = 0
+    #print(preds_size.data[0])
+    out = ''
+    while i < preds_size.data[0]:
+        if preds.data[i] is not 0:
+		    out += alphabet[preds.data[i] - 1]
+        i += 1
+    print(out)
+    #print(preds)
+    #sim_pred = converter.decode(preds.data, preds_size.data, raw=False)
+    #print('results: {0}'.format(sim_pred))
 
 
 if __name__ == '__main__':
@@ -62,7 +92,7 @@ if __name__ == '__main__':
     if torch.cuda.is_available():
         model = model.cuda()
     print('loading pretrained model from {0}'.format(crnn_model_path))
-    # 导入已经训练好的crnn模型
+    #
     model.load_state_dict(torch.load(crnn_model_path))
     
     started = time.time()
